a3d9d3f41eb6ecceed45665af6812c1bd4a8210d
tsoareu <karahmaa25@gmail.com>
2024-09-06 09:43:34 +0700

    01

---
+++ 01/MyFirstJava08.java
@@ -0,0 +1,12 @@
+public class MyFirstJava08 {
+    public static void main(String[]args) {
+
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama    : Kartika Rahma Nuraini");
+        System.out.println("Absen   : 08");
+        System.out.println("Kelas   : 1D");
+        System.out.println("Prodi   : Sistem Informasi Bisnis");
+        System.out.println("Jurusan : Teknologi Informasi");
+
+    }
+}
